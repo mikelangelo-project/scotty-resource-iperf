c108a664a3a765c7142adc4de57eb0496c047592
Maik Srba <msrba@scotty.mikelangelo.gwdg.de>
2017-12-08 17:57:19 +0100

    change bash for scotty user and fix sshd_config

--- templates/fragments/create_user_scotty.sh
+++ templates/fragments/create_user_scotty.sh
@@ -4,8 +4,11 @@
 
 echo "Creating $SCOTTY_USER user"
 groupadd $SCOTTY_USER
-useradd $SCOTTY_USER -g $SCOTTY_USER -G sudo --create-home
+useradd $SCOTTY_USER -g $SCOTTY_USER -G sudo --shell /bin/bash --create-home
 echo "${SCOTTY_USER}:${SCOTTY_PASSWORD}" | chpasswd
 
 echo "scotty ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-sudo-scotty-without-password
-echo "AllowUsers scotty"   >> /etc/ssh/sshd_config
+echo "" >> /etc/ssh/sshd_config
+echo "AllowUsers scotty" >> /etc/ssh/sshd_config
+
+systemctl restart sshd
